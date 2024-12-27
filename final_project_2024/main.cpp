01101ab1998a67618bed1d2a9f2529ef53d4bd83
BethTsai <146188828+BethTsai@users.noreply.github.com>
2024-12-27 21:54:05 +0800

    test output

--- final_project_2024/main.cpp
+++ final_project_2024/main.cpp
@@ -56,8 +56,14 @@ int main(int argc, char *argv[])
 	string query = string(argv[2]);
 	string output = string(argv[3]);
 
-	// Read File & Parser Example
+	// Open output file
+	ofstream outputFile(output);
+	if(!outputFile.is_open()){
+		cout << "Error to open output file\n";
+		return 0;
+	}
 
+	// Read File & Parser Example
 
 	string file, title_name, tmp;
 	fstream fi;
@@ -65,7 +71,7 @@ int main(int argc, char *argv[])
 
 	// from data_dir get file ....
 	// eg : use 0.txt in data directory
-	fi.open("data/0.txt", ios::in);
+	fi.open("data/20.txt", ios::in);
 
     // GET TITLENAME
 	getline(fi, title_name);
@@ -75,9 +81,9 @@ int main(int argc, char *argv[])
 
 	vector<string> title = word_parse(tmp_string);
 
-	// for(auto &word : title){
-	// 	cout << word << endl;
-	// }
+	for(auto &word : title){
+		outputFile << word << endl;
+	}
 
     // GET CONTENT LINE BY LINE
 	while(getline(fi, tmp)){
@@ -89,13 +95,14 @@ int main(int argc, char *argv[])
 		vector<string> content = word_parse(tmp_string);
 
 		for(auto &word : content){
-			cout << word << endl;
+			outputFile << word << endl;
 		}
 		//......
 	}
 
     // CLOSE FILE
 	fi.close();
+	outputFile.close();
 }
 
 
